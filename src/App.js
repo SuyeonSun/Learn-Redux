88e509a26dad3c08e765e81df2fb9eb2556bc080
SuyeonSun <suninisy@gmail.com>
2022-01-13 16:34:24 +0900

    project start

--- src/App.js
+++ src/App.js
@@ -1,48 +1,66 @@
 import React, {useState} from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  let [title, setTitle] = useState(['글제목1', '글제목2', '글제목3']);
-  let [modal, setModal] = useState(false);
+  // useState 배열 안에 객체
+  // const [items, setItems] = useState([
+  //   { itemName: 'item 1', quantity: 1, isSelected: false },
+  //   { itemName: 'item 2', quantity: 3, isSelected: true },
+  //   { itemName: 'item 3', quantity: 2, isSelected: false },
+  // ]);
+  let [items, setItems] = useState([{itemName: 'item1', quantity: 1, isSelected: false}]);
+  let [inputValue, setInputValue] = useState('');
 
-  let [누른제목, 누른제목변경] = useState(0);
+  let handleAddButtonClick = () => {
+    const newItem = {
+      itemName : inputValue,
+      
+    }
+  }
 
   return (
-    <div className="App">
-      <div className="black-nav">
-        <div>개발 블로그</div>
+    <div className='app-background'>
+      <div className='main-container'>
+        <div className='add-item-box'>
+          <input className='add-item-input' placeholder='장 볼 상품을 입력하세요!'
+          value={inputValue} onChange={(e) => setInputValue(e.target.value)}
+          ></input>
+          <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+        </div>
+        <div className='item-list'>
+          {items.map((item, idx) => (
+          <div className='item-container'>
+            <div className='item-name'>
+              {item.isSelected ? (
+                <>
+                  <FontAwesomeIcon icon={faCheckCircle}></FontAwesomeIcon>
+                  <span className='completed'>{item.itemName}</span>
+                </>
+              ) : (
+                <>
+                <FontAwesomeIcon icon={faCircle}></FontAwesomeIcon>
+                <span>{item.itemName}</span>
+                </>
+              )}
+            </div>
+            <div className='quantity'>
+              <button>
+                <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
+              </button>
+              <span>1</span>
+              <button>
+                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
-
-      {
-        title.map(function(element, i){
-          return (
-            <div onClick={()=>{누른제목변경(i)}}>{element}</div>
-          )
-        })
-      }
-
-      <button onClick={()=>{누른제목변경(0)}}>1</button>
-      <button onClick={()=>{누른제목변경(1)}}>2</button>
-      <button onClick={()=>{누른제목변경(2)}}>3</button>
-
-      {/* button */}
-      <button onClick={() => {setModal(!modal)}}>열고닫기</button>
-      {
-        modal === true
-        ? <Modal title={title} 누른제목={누른제목}></Modal>
-        : null
-      }
-
-    </div> 
-  );
-}
-
-function Modal(props) {
-  return(
-    <div className="modal">
-      {props.title[props.누른제목]}
+        <div className='total'>Total : 6</div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
