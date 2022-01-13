df9fd96a583b1e0b7d73fa046335856fcb15bbee
SuyeonSun <suninisy@gmail.com>
2022-01-13 18:26:59 +0900

    app.js 완성

--- src/App.js
+++ src/App.js
@@ -1,38 +1,81 @@
 import React, {useState} from 'react';
-import './App.css';
+import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   // useState 배열 안에 객체
-  // const [items, setItems] = useState([
-  //   { itemName: 'item 1', quantity: 1, isSelected: false },
-  //   { itemName: 'item 2', quantity: 3, isSelected: true },
-  //   { itemName: 'item 3', quantity: 2, isSelected: false },
-  // ]);
-  let [items, setItems] = useState([{itemName: 'item1', quantity: 1, isSelected: false}]);
+  let [items, setItems] = useState([]);
   let [inputValue, setInputValue] = useState('');
 
+  // 개수
+  let [totalItemCount, setTotalItemCount] = useState(0);
+
+  // 함수 handleAddButtonClick
   let handleAddButtonClick = () => {
     const newItem = {
       itemName : inputValue,
-      
-    }
+      quantity: 1,
+      isSelected: false,
+    };
+    const newItems = [...items, newItem]; // items를 newItems에 deep copy
+    setItems(newItems);
+    setInputValue(''); // Input창 초기화
+
+    calculateTotal();
   }
+
+  // 함수 handleQuantityIncrease
+  const handleQuantityIncrease = (index) => {
+    const newItems = [...items];
+    newItems[index].quantity++;
+    setItems(newItems);
+
+    calculateTotal();
+  }
+
+  // 함수 handleQuantityDecrease
+  const handleQuantityDecrease = (index) => {
+    const newItems = [...items];
+    newItems[index].quantity--;
+    setItems(newItems);
+
+    calculateTotal();
+  }
+
+  // 함수 toggleComplete 
+  const toggleComplete = (index) => {
+    const newItems = [...items];
+    newItems[index].isSelected = !newItems[index].isSelected;
+    setItems(newItems);
+  }
+
+  // 함수 calculateTotal
+	const calculateTotal = () => {
+		const totalItemCount = items.reduce((total, item) => {
+			return total + item.quantity;
+		}, 0);
+
+		setTotalItemCount(totalItemCount);
+	};
 
   return (
     <div className='app-background'>
       <div className='main-container'>
+
+        {/* input 값 입력 */}
         <div className='add-item-box'>
           <input className='add-item-input' placeholder='장 볼 상품을 입력하세요!'
           value={inputValue} onChange={(e) => setInputValue(e.target.value)}
           ></input>
-          <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+          <FontAwesomeIcon icon={faPlus} onClick={()=>handleAddButtonClick()}></FontAwesomeIcon>
         </div>
+
+        {/* list 보여주기 */}
         <div className='item-list'>
           {items.map((item, idx) => (
           <div className='item-container'>
-            <div className='item-name'>
+            <div className='item-name' onClick={()=>toggleComplete(idx)}>
               {item.isSelected ? (
                 <>
                   <FontAwesomeIcon icon={faCheckCircle}></FontAwesomeIcon>
@@ -45,19 +88,21 @@ function App() {
                 </>
               )}
             </div>
+            {/* 수량 */}
             <div className='quantity'>
               <button>
-                <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faChevronLeft} onClick={() => handleQuantityDecrease(idx)}></FontAwesomeIcon>
               </button>
-              <span>1</span>
+              <span>{item.quantity}</span>
               <button>
-                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faChevronRight} onClick={()=> handleQuantityIncrease(idx)}></FontAwesomeIcon>
               </button>
             </div>
           </div>
         ))}
+      {/* 총 수량 */}
       </div>
-        <div className='total'>Total : 6</div>
+        <div className='total'>Total : {totalItemCount}</div>
       </div>
     </div>
   );
